ec4d4d0073fe32862cc2680380b4dab82d25c429
Sasa <sasastanisic4@gmail.com>
2023-08-23 14:52:44 +0200

    Finished testing data access layer

--- app/src/test/java/rs/ac/metropolitan/data/data_source/TravelDaoTest.kt
+++ app/src/test/java/rs/ac/metropolitan/data/data_source/TravelDaoTest.kt
@@ -2,6 +2,8 @@ package rs.ac.metropolitan.data.data_source
 
 import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
@@ -24,13 +26,57 @@ class TravelDaoTest {
     }
 
     @Test
-    fun shouldInsertTravel() = runTest {
-        val travel = Travel(0, "", "", "", 0.0f, "")
+    fun testUpsertTravel() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
         coEvery { travelDao.upsertTravel(travel) } just runs
 
         travelDao.upsertTravel(travel)
-
         coVerify { travelDao.upsertTravel(travel) }
+    }
+
+    @Test
+    fun testGetAllTravels() = runTest {
+        val travels = listOf(
+            Travel(
+                1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+                "One of the most beautiful cities in the entire world"
+            ),
+            Travel(
+                2, "Los Angeles", "USA", "Hollywood Apartment", 3500.0f,
+                "City of angels"
+            )
+        )
+        every { travelDao.getAllTravels() } returns flowOf(travels)
+
+        val result = travelDao.getAllTravels().first()
+        assert(result == travels)
+    }
+
+    @Test
+    fun testGetTravelById() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
+        coEvery { travelDao.getTravelById(1) } returns travel
+
+        val result = travelDao.getTravelById(1)
+        assert(result == travel)
+    }
+
+    @Test
+    fun testDeleteTravel() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
+        coEvery { travelDao.deleteTravel(travel) } just runs
+
+        travelDao.deleteTravel(travel)
+        coVerify { travelDao.deleteTravel(travel) }
     }
 
 }
--- app/src/test/java/rs/ac/metropolitan/data/repository/TravelRepositoryTest.kt
+++ app/src/test/java/rs/ac/metropolitan/data/repository/TravelRepositoryTest.kt
@@ -1,2 +1,86 @@
 package rs.ac.metropolitan.data.repository
 
+import io.mockk.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import rs.ac.metropolitan.data.data_source.TravelDao
+import rs.ac.metropolitan.domain.model.Travel
+
+@ExperimentalCoroutinesApi
+class TravelRepositoryTest {
+
+    private val travelDao = mockk<TravelDao>()
+    private val travelRepository = TravelRepositoryImpl(travelDao)
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testUpsertTravel() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
+        coEvery { travelDao.upsertTravel(travel) } just runs
+
+        travelRepository.upsertTravel(travel)
+        coVerify { travelDao.upsertTravel(travel) }
+        coVerify { travelRepository.upsertTravel(travel) }
+    }
+
+    @Test
+    fun testGetAllTravels() = runTest {
+        val travels = listOf(
+            Travel(
+                1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+                "One of the most beautiful cities in the entire world"
+            ),
+            Travel(
+                2, "Los Angeles", "USA", "Hollywood Apartment", 3500.0f,
+                "City of angels"
+            )
+        )
+        every { travelDao.getAllTravels() } returns flowOf(travels)
+
+        val result = travelRepository.getAllTravels().first()
+        assert(result == travels)
+    }
+
+    @Test
+    fun testGetTravelById() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
+        coEvery { travelDao.getTravelById(1) } returns travel
+
+        val result = travelRepository.getTravelById(1)
+        assert(result == travel)
+    }
+
+    @Test
+    fun testDeleteTravel() = runTest {
+        val travel = Travel(
+            1, "Venice", "Italy", "Hotel Blue", 1000.0f,
+            "One of the most beautiful cities in the entire world"
+        )
+        coEvery { travelDao.deleteTravel(travel) } just runs
+
+        travelRepository.deleteTravel(travel)
+        coVerify { travelDao.deleteTravel(travel) }
+        coVerify { travelRepository.deleteTravel(travel) }
+    }
+
+}
